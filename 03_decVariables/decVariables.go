27cf7ef64c6a52028b829266c9661c6d119c4e17
David <99davidpaul@gmail.com>
2018-07-11 14:33:53 -1000

    used diff var declarations

---
+++ 03_decVariables/decVariables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "David"
+	fmt.Println(name)
+
+	var name1 = "Paul"
+	fmt.Println(name1)
+
+	var name2, name3 = "Cruz", "Tupper"
+	fmt.Println(name2, name3)
+}
