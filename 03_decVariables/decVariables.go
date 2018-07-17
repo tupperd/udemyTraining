1b3e535f9bd128a6ecdf5ab491199c45f061afd0
David <99davidpaul@gmail.com>
2018-07-17 12:28:18 -1000

    S

--- 03_decVariables/decVariables.go
+++ 03_decVariables/decVariables.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
+const p = "hello"
+
 func main() {
-	name := "David"
-	fmt.Println(name)
-
-	var name1 = "Paul"
-	fmt.Println(name1)
-
-	var name2, name3 = "Cruz", "Tupper"
-	fmt.Println(name2, name3)
-
-	var test bool
-	fmt.Println(test)
+	fmt.Println(p)
+	const fail = "10"
+	fmt.Println(fail)
 }
